edb7123e52132b78d0d480a0d49e7fcb84be88d9
OfekSagiv <ofeksagiv@gmail.com>
2025-04-30 18:07:09 +0300

    fix: added try catch block to gett all purchases

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -3,4 +3,5 @@ module.exports = {
   INVALID_INPUT: 'Invalid input data',
   PRODUCT_NOT_FOUND: 'Product not found',
   PURCHASE_NOT_FOUND: 'Purchase not found',
+  PURCHASES_FETCH_FAILED: 'Failed to retrieve purchases',
 };
