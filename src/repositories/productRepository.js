e4c9072d33e5951d11abd4982398559b32775816
OfekSagiv <ofeksagiv@gmail.com>
2025-04-30 14:23:03 +0300

    Add Product entity with full MVC structure and constants integration

---
+++ src/repositories/productRepository.js
@@ -0,0 +1,23 @@
+const Product = require('../models/productModel');
+
+const createProduct = (productData) => Product.create(productData);
+
+const getAllProducts = () => Product.find();
+
+const getProductById = (id) => Product.findById(id);
+
+const updateProduct = (id, updateData) =>
+  Product.findByIdAndUpdate(id, updateData, {
+    new: true,
+    runValidators: true,
+  });
+
+const deleteProduct = (id) => Product.findByIdAndDelete(id);
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+};
