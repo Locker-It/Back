8d29557a6dadd9877ed4e934d6a6434d3778e085
OfekSagiv <ofeksagiv@gmail.com>
2025-04-30 10:32:04 +0300

    Integrate Swagger with config separation and production check

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const api = require('./routes/apiRouter');
+const setupSwagger = require('./config/swagger');
 
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV !== 'production') {
+    setupSwagger(app);
+}
+
 app.use('/api/v1', api)
 
 module.exports = app;
