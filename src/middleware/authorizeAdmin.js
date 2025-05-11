b3407b10ea7ea023b1f71ac1cb5420c684af0eb3
OfekSagiv <ofeksagiv@gmail.com>
2025-05-11 21:13:58 +0300

    fixed code

--- src/middleware/authorizeAdmin.js
+++ src/middleware/authorizeAdmin.js
@@ -1,12 +1,14 @@
+const { StatusCodes } = require('http-status-codes');
 const errorMessages = require('../constants/errorMessages');
+const ROLES = require('../constants/roles');
 
 const authorizeAdmin = (req, res, next) => {
   if (!req.user) {
-    return res.status(401).json({ error: errorMessages.UNAUTHORIZED });
+    return res.status(StatusCodes.UNAUTHORIZED).json({ error: errorMessages.UNAUTHORIZED });
   }
 
-  if (req.user.role !== 'admin') {
-    return res.status(403).json({ error: errorMessages.FORBIDDEN });
+  if (req.user.role !== ROLES.ADMIN) {
+    return res.status(StatusCodes.FORBIDDEN).json({ error: errorMessages.FORBIDDEN });
   }
 
   return next();
