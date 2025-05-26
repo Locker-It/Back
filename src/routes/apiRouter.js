7bd1dbb2d7dfd8ea2d7ab55f0993b91bc5455357
Chen-kaduri <chenkaduri@gmail.com>
2025-05-26 23:46:37 +0300

    Create full rest for available lockers in the backend

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -7,12 +7,15 @@ const productRoutes = require('./productRouter');
 const purchaseRouter = require('./purchaseRouter');
 const authRoutes = require('./authRoutes');
 const lockerRoutes = require('./lockerRouter');
+const availableLockerRoutes = require('./availableLockerRouter');
+const { AVAILABLE_LOCKERS_BASE } = require('../constants/apiPaths');
 
 const {
   USERS_BASE,
   PRODUCTS_BASE,
   PURCHASES_BASE,
   AUTH_BASE,
+
 } = require('../constants/apiPaths');
 
 
@@ -23,5 +26,6 @@ router.use(PRODUCTS_BASE, productRoutes);
 router.use(PURCHASES_BASE, purchaseRouter);
 router.use(AUTH_BASE, authRoutes);
 router.use(LOCKERS_BASE, lockerRoutes);
+
 
 module.exports = router;
