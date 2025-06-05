3f02465fb8a8831628650d4d68cf7def70290046
OfekSagiv <ofeksagiv@gmail.com>
2025-06-05 18:56:37 +0300

    fix comments

--- src/models/availableLockerModel.js
+++ src/models/availableLockerModel.js
@@ -3,17 +3,13 @@ const MODEL_NAMES = require('../constants/modelNames');
 
 const availableLockerSchema = new mongoose.Schema(
   {
-    id: {
-      type: Number,
-      required: true,
-      unique: true,
-    },
     productId: {
       type: Number,
       required: true,
     },
-    lockerId: {
-      type: Number,
+    locker: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: MODEL_NAMES.LOCKER,
       required: true,
     },
   },
@@ -21,4 +17,3 @@ const availableLockerSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model(MODEL_NAMES.AVAILABLE_LOCKER, availableLockerSchema);
-
--- src/repositories/availableLockerRepository.js
+++ src/repositories/availableLockerRepository.js
@@ -1,13 +1,17 @@
 const AvailableLocker = require('../models/availableLockerModel');
+const { LOCKER } = require('../constants/repositoryModel');
 
 const createAvailableLocker = (data) => AvailableLocker.create(data);
 
-const getAllAvailableLockers = () => AvailableLocker.find();
+const getAllAvailableLockers = () => AvailableLocker.find().populate(LOCKER);
 
-const getAvailableLockerById = (id) => AvailableLocker.findById(id);
+const getAvailableLockerById = (id) => AvailableLocker.findById(id).populate(LOCKER);
 
 const updateAvailableLocker = (id, data) =>
-  AvailableLocker.findByIdAndUpdate(id, data, { new: true, runValidators: true });
+  AvailableLocker.findByIdAndUpdate(id, data, {
+    new: true,
+    runValidators: true,
+  }).populate(LOCKER);
 
 const deleteAvailableLocker = (id) => AvailableLocker.findByIdAndDelete(id);
 
