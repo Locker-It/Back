d085fb62f8005198761e80097ec99ae162cfab39
OfekSagiv <ofeksagiv@gmail.com>
2025-06-20 13:07:03 +0300

    fixed issues

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const userController = require('../controllers/userController');
-const { LOGIN, REGISTER,REFRESH,LOGOUT, CURRENT_USER } = require('../constants/apiPaths');
+const {
+  LOGIN,
+  REGISTER,
+  REFRESH,
+  LOGOUT,
+  CURRENT_USER,
+} = require('../constants/apiPaths');
 const { getCurrentUser } = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
 
